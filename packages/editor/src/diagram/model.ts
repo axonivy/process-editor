1c488b31e38d93815553095a043a796e1382f99f
Lukas Lieb <lukas.lieb@axonivy.com>
2025-08-22 08:48:52 +0200

    Fix do not allow move lanes via mouse dnd

--- packages/editor/src/diagram/model.ts
+++ packages/editor/src/diagram/model.ts
@@ -60,7 +60,6 @@ export class LaneNode extends RectangularNode implements WithEditableLabel, Args
     selectFeature,
     quickActionFeature,
     deletableFeature,
-    moveFeature,
     resizeFeature
   ];
 
