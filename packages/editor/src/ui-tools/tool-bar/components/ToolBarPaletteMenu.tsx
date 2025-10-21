2ce16323fa29f0b332982323fff60a7a145f9ae2
Lukas Lieb <lukas.lieb@axonivy.com>
2025-10-21 16:05:47 +0200

    Fix extension palette
    The react ui refactoring broke the extensions palette items as list.

--- packages/editor/src/ui-tools/tool-bar/components/ToolBarPaletteMenu.tsx
+++ packages/editor/src/ui-tools/tool-bar/components/ToolBarPaletteMenu.tsx
@@ -1,11 +1,9 @@
 import { cn, Flex, IvyIcon, Palette, PaletteSection, type PaletteItemConfig, type PaletteItemProps } from '@axonivy/ui-components';
-import type { IvyIcons } from '@axonivy/ui-icons';
 import { type IActionDispatcher } from '@eclipse-glsp/client';
 import { t } from 'i18next';
 import React from 'react';
 import { ActivityTypes, EventIntermediateTypes, EventStartTypes } from '../../../diagram/view-types';
 import { paletteItemsToSections, type MenuPaletteItem } from '../../../utils/menu-utils';
-import { createIcon } from '../../../utils/ui-utils';
 import { MenuIcons } from '../../menu/icons';
 import { ShowToolBarMenuAction } from '../tool-bar-menu';
 
@@ -16,7 +14,8 @@ interface ToolBarPaletteMenuProps {
 }
 
 export type ToolPaletteItemConfig = PaletteItemConfig & {
-  icon: string | React.ReactNode /* items may specify image path so we need to translate into img */;
+  paletteIcon: React.ReactNode;
+  info?: string;
 };
 
 export const ToolBarPaletteMenu: React.FC<ToolBarPaletteMenuProps> = ({ paletteItems, menuAction, actionDispatcher }) => {
@@ -28,64 +27,81 @@ export const ToolBarPaletteMenu: React.FC<ToolBarPaletteMenuProps> = ({ paletteI
     [actionDispatcher, menuAction]
   );
 
-  const sections = React.useMemo(() => {
-    return paletteItemsToSections(
-      paletteItems,
-      item =>
-        ({
-          name: item.label + (item.info || ''),
-          description: item.description || item.label,
-          icon: createPaletteIcon(item),
-          onClick: async () => onItemSelected(item)
-        }) as ToolPaletteItemConfig
-    );
-  }, [paletteItems, onItemSelected]);
+  let horizontal = false;
+  if (menuAction.id === 'extensions_menu') {
+    horizontal = true;
+  }
+
+  const sections = React.useMemo(
+    () =>
+      paletteItemsToSections<MenuPaletteItem, ToolPaletteItemConfig>(paletteItems, item => ({
+        name: item.label,
+        description: item.description || item.label,
+        paletteIcon: <PaletteIcon item={item} />,
+        info: item.info,
+        onClick: async () => onItemSelected(item)
+      })),
+    [paletteItems, onItemSelected]
+  );
 
   return (
     <Palette sections={sections} options={{ searchPlaceholder: t('a11y.search.placeholder'), emptyMessage: t('label.empty') }}>
       {(title, items) => (
         <PaletteSection key={title} title={title} items={items}>
-          {item => <ToolPaletteItem key={item.name} {...item} />}
+          {item => <ToolPaletteItem key={item.name} {...item} horizontal={horizontal} />}
         </PaletteSection>
       )}
     </Palette>
   );
 };
 
-function createPaletteIcon(item: MenuPaletteItem): IvyIcons | React.ReactNode | HTMLElement {
+const PaletteIcon = ({ item }: { item: MenuPaletteItem }) => {
   if (!item.icon) {
-    return createIcon();
+    return <i />;
   }
-  if (item.icon.startsWith(ActivityTypes.THIRD_PARTY)) {
-    return MenuIcons.get(ActivityTypes.THIRD_PARTY);
-  }
-  if (item.icon.startsWith(EventStartTypes.START_THIRD_PARTY)) {
-    return MenuIcons.get(EventStartTypes.START_THIRD_PARTY);
-  }
-  if (item.icon.startsWith(EventIntermediateTypes.INTERMEDIATE_THIRD_PARTY)) {
-    return MenuIcons.get(EventIntermediateTypes.INTERMEDIATE_THIRD_PARTY);
-  }
-  const knownIcon = MenuIcons.get(item.icon);
-  if (knownIcon) {
-    return knownIcon;
+  const ivyIcon = createPaletteIcon(item.icon);
+  if (ivyIcon) {
+    return <IvyIcon icon={ivyIcon} style={{ textAlign: 'center', fontSize: 24 }} />;
   }
   return (
     <span className='sprotty-icon'>
       <img src={item.icon} alt={item.label} />
     </span>
   );
+};
+
+function createPaletteIcon(icon: string) {
+  if (icon.startsWith(ActivityTypes.THIRD_PARTY)) {
+    return MenuIcons.get(ActivityTypes.THIRD_PARTY);
+  }
+  if (icon.startsWith(EventStartTypes.START_THIRD_PARTY)) {
+    return MenuIcons.get(EventStartTypes.START_THIRD_PARTY);
+  }
+  if (icon.startsWith(EventIntermediateTypes.INTERMEDIATE_THIRD_PARTY)) {
+    return MenuIcons.get(EventIntermediateTypes.INTERMEDIATE_THIRD_PARTY);
+  }
+  return MenuIcons.get(icon);
 }
 
-// Extended PaletteItem so that we can render images as icons
+type ToolPaletteItemProps = PaletteItemProps<ToolPaletteItemConfig> & {
+  horizontal?: boolean;
+};
 
-const ToolPaletteItem = ({ name, description, icon, onClick, classNames }: PaletteItemProps<ToolPaletteItemConfig>) => (
-  <button className={cn(classNames.paletteItem, 'ui-palette-item')} onClick={onClick} title={description}>
-    <Flex direction='column' gap={1} alignItems='center'>
+const ToolPaletteItem = ({ name, description, paletteIcon, info, onClick, classNames, horizontal }: ToolPaletteItemProps) => (
+  <button
+    className={cn(classNames.paletteItem, 'ui-palette-item')}
+    onClick={onClick}
+    title={description}
+    data-style={horizontal ? 'horizontal' : 'vertical'}
+  >
+    <Flex gap={1} alignItems='center' className='ui-palette-item-content'>
       <Flex className={cn(classNames.paletteItemIcon, 'ui-palette-item-icon')} justifyContent='center' alignItems='center'>
-        {/* Need to manually set style of icon as the 'paletteItemIvyIcon' icon class name cannot be imported properly. */}
-        {typeof icon === 'string' ? <IvyIcon icon={icon} style={{ textAlign: 'center', fontSize: 24 }} /> : icon}
+        {paletteIcon}
       </Flex>
-      <Flex justifyContent='center'>{name}</Flex>
+      <Flex justifyContent='center' gap={1} className='ui-palette-item-text'>
+        <span className='ui-palette-item-label'>{name}</span>
+        {info && <span className='ui-palette-item-label ui-palette-item-info'>{info}</span>}
+      </Flex>
     </Flex>
   </button>
 );
