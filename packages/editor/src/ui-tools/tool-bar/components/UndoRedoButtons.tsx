9e1e81a006cbc8649e7a9e6312bb5224f58e032a
Lukas Lieb <lukas.lieb@axonivy.com>
2025-12-22 14:13:12 +0100

    Fix undo redo labels

--- packages/editor/src/ui-tools/tool-bar/components/UndoRedoButtons.tsx
+++ packages/editor/src/ui-tools/tool-bar/components/UndoRedoButtons.tsx
@@ -1,8 +1,8 @@
 import { Button, Flex, hotkeyText, isWindows, Separator, ToolbarContainer } from '@axonivy/ui-components';
 import { IvyIcons } from '@axonivy/ui-icons';
 import { GArgument, GModelRoot, type IActionDispatcher, RedoAction, UndoAction } from '@eclipse-glsp/client';
+import { t } from 'i18next';
 import React from 'react';
-import { useTranslation } from 'react-i18next';
 
 interface UndoRedoButtonsProps {
   actionDispatcher: IActionDispatcher;
@@ -10,16 +10,10 @@ interface UndoRedoButtonsProps {
 }
 
 export const UndoRedoButtons = ({ actionDispatcher, modelRoot }: UndoRedoButtonsProps) => {
-  const { t } = useTranslation();
   const canUndo = GArgument.getArgument(modelRoot, 'canUndo') === true;
   const canRedo = GArgument.getArgument(modelRoot, 'canRedo') === true;
-  const { undoLabel, redoLabel } = React.useMemo(
-    () => ({
-      undoLabel: t('common.hotkey.undo', { hotkey: hotkeyText('mod+Z') }),
-      redoLabel: t('common.hotkey.redo', { hotkey: hotkeyText(isWindows() ? 'mod+Y' : 'mod+shift+Z') })
-    }),
-    [t]
-  );
+  const undoLabel = t('common.hotkey.undo', { hotkey: hotkeyText('mod+Z') });
+  const redoLabel = t('common.hotkey.redo', { hotkey: hotkeyText(isWindows() ? 'mod+Y' : 'mod+shift+Z') });
 
   return (
     <ToolbarContainer maxWidth={450}>
