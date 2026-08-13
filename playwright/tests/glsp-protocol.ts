a3f98ce105d431616ffd283d193e958316a802f5
Louis <louis.mueller@axonivy.com>
2026-08-13 15:46:28 +0200

    www

--- playwright/tests/glsp-protocol.ts
+++ playwright/tests/glsp-protocol.ts
@@ -3,7 +3,7 @@ import type { ElementType } from '@axonivy/process-editor-inscription-protocol';
 import { Client, RequestManager, WebSocketTransport } from '@open-rpc/client-js';
 import { randomUUID } from 'crypto';
 
-const server = (process.env.BASE_URL ? `${process.env.BASE_URL}` : 'http://localhost:8080') + '/~Developer-inscription-test-project';
+const server = (process.env.BASE_URL ? `${process.env.BASE_URL}` : 'http://localhost:8080/') + '~Developer-inscription-test-project';
 const app = 'Developer-inscription-test-project';
 const project = 'inscription-test-project';
 
--- playwright/tests/page-objects/inscription/inscription-view.ts
+++ playwright/tests/page-objects/inscription/inscription-view.ts
@@ -3,7 +3,7 @@ import { expect } from '@playwright/test';
 import { InscriptionTab } from './inscription-tab';
 import { Outline } from './outline';
 
-const server = (process.env.BASE_URL ? `${process.env.BASE_URL}` : 'localhost:8080') + '/~Developer-inscription-test-project';
+const server = (process.env.BASE_URL ? `${process.env.BASE_URL}` : 'localhost:8080/') + '~Developer-inscription-test-project';
 const app = 'Developer-inscription-test-project';
 const project = 'inscription-test-project';
 
