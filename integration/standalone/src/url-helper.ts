bf920803839eb885f983ab8b69698c8441b41c4c
Lukas Lieb <lukas.lieb@axonivy.com>
2026-08-26 18:07:42 +0200

    Move glsp ws to security endpoint

--- integration/standalone/src/url-helper.ts
+++ integration/standalone/src/url-helper.ts
@@ -1,12 +1,3 @@
-export function getServerDomain(): string {
-  const protocol = window.location.protocol;
-  if (protocol.startsWith('http')) {
-    const href = window.location.href;
-    return href.substring(protocol.length + 2, href.indexOf('/process-editor'));
-  }
-  return 'localhost:8081';
-}
-
 export function isSecureConnection(): boolean {
   return window.location.protocol === 'https:' || getParameters().get('secure') === 'true';
 }
